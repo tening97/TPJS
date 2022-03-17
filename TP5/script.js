f85aa7e5ea800adc61ed3046a4e772ba732baf9f
teningNgom <teningngom97@gmail.com>
2022-03-17 16:33:18 +0000

    TP5

---
+++ TP5/script.js
@@ -0,0 +1,38 @@
+
+const buttons = document.querySelectorAll('button');
+const generate = document.querySelector('.generate');
+
+/* buttons[0].addEventListener('click', function () {
+    generer(' success', 'Mon premier projet');
+}) */
+
+/* buttons[1].addEventListener('click', function () {
+    generer('danger', 'Action dangereuse');
+
+})
+buttons[2].addEventListener('click', function () {
+    generer('warning', 'Attention');
+})
+buttons[3].addEventListener('click', function () {
+    generer('info', 'Information');
+})
+ */
+function generer(type, text) {
+    const div = document.createElement('div');
+    const btngenerer = document.createElement('button');
+    btngenerer.setAttribute('class', `${type}`)
+    generate.appendChild(btngenerer)
+    btngenerer.innerHTML = `${text}`;
+    setTimeout(function () {
+        btngenerer.remove();
+
+    }, 1000)
+
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        generer(button.classList[0], button.innerText)
+    })
+
+});
